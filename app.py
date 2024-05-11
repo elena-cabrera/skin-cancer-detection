263cb9cb49aa758c039f2c57972a26404e717e8a
elena-cabrera <elenacabreracasquet@gmail.com>
2024-05-11 12:53:05 +0200

    Add prediction with Resnet152 to streamlit app

--- app.py
+++ app.py
@@ -1,5 +1,44 @@
 import streamlit as st
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
 
+MODELS = {
+    "CNN from Scratch": {
+        "path": "models/cnn_scratch-40.h5",
+        "size": (128, 128)
+    },
+    "Transfer Learning (VGG19)": {
+        "path": "models/cnn_vgg19-100.h5",
+        "size": (224, 224)
+    },
+    "Transfer Learning (ResNet151)": {
+        "path": "models/ResNet152_0.001_5.h5",
+        "size": (224, 224)
+    }
+}
+
+def predict(model_name, img):
+    # Load the model
+    print(f"Loading model: {model_name}")
+    model_path = MODELS[model_name]["path"]
+    model = load_model(model_path)
+
+    # Load the image with size depending on the model
+    print("Loading image")
+    img_size = MODELS[model_name]["size"]
+    img = image.load_img(img, target_size=img_size)
+    print("Image loaded, resizing and normalizing the image")
+    img_array = image.img_to_array(img)
+    img_array = img_array / 255.0
+    img_array = img_array.reshape(1, *img_size, 3)
+
+    # Predict
+    print("Predicting the image class")
+    prediction = model.predict(img_array)
+    prediction = "Benign" if prediction < 0.5 else "Melanoma"
+    print(f"Prediction: {prediction}")
+
+    return prediction
 
 def main():
     # Favicon
@@ -21,13 +60,20 @@ def main():
         with col2:
             st.image(uploaded_file, caption="Uploaded Image", use_column_width=False)
 
+        # Select the model
+        model_name = st.selectbox("Select a model", list(MODELS.keys()))
+
         # Predict button
         if st.button("🤖 Predict with AI", type="primary"):
-            # Display the prediction
-            st.write("⏳ Analyzing your image with Artificial Intelligence...")
+            # Placeholder
+            placeholder = st.empty()
+            placeholder.write("⏳ Analyzing your image with Artificial Intelligence...")
+
+            # Predict
+            prediction = predict(model_name, uploaded_file)
+            placeholder.empty()
 
             # Prediction
-            prediction = "Melanoma"
             if prediction == "Benign":
                 st.write(f"✅ **{prediction}**")
                 st.write("This skin mole does not appear to be cancerous. However, the AI model is not 100% accurate. Please consult a dermatologist.")
